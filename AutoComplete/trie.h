4e168e91e4f0b6fc3365579e26bfd348878d65dc
Arya Nguyen <quynhnhu3132000@gmail.com>
2020-04-16 16:47:49 -0400

    Update trie.h

--- AutoComplete/trie.h
+++ AutoComplete/trie.h
@@ -1,52 +1,44 @@
 /****************************************************
+ * trie.h
  * 
  * Description: Trie Data Structure
  * Author: Arya Nguyen
  * Project: EyeTracking_SpeechGenerating
  * 
  ****************************************************/
+#ifndef TRIE_H
+#define TRIE_H
+
 #include <iostream>
 #include "node.h"
 
 class Trie
 {
+    // class Node can access private memebers of Trie
     friend class Node;
-    
+
 private:
-    bool space;           // string stores user query with space
-    std::string noPrefix; // string that stores user query without prefix
-    Node* root;
+    bool space;           // true if space in string storing user query
+    std::string noPrefix; // string storing user query without prefix
+    Node *root;
 
 public:
-    Trie()
+    Trie() // Constructor
     {
         root = new Node();
         this->space = false;
-    };
-    ~Trie(){};
+    }
+    ~Trie() {} // Destructor
 
-    // search quey for space
-    void isSpace(std::string data);
-
-    // return space
-    bool getSpace() { return space; }
-
-    // insert data into tree
-    void insert(std::string data);
-
-    // search key in tree
-    void search(std::string data);
-
-    // print suggested queries
-    void print_tree(Node* root, std::string data, std::string str);
-    void print_tree(Node* root, std::string data);
-
-    // remove prefix, if space in string
-    void remove_prefix(std::string data);
-
-    // return noPrefix
-    std::string getNo_prefix() { return noPrefix; };
-
-    // returns prefix
-    std::string break_string(std::string){};
+    void isSpace(std::string data);                                 // search quey for space, set bool space
+    bool getSpace() { return space; }                               // return space
+    void insert(std::string data);                                  // insert data into tree
+    void search(std::string data);                                  // search key in tree
+    void print_tree(Node *root, std::string data, std::string str); // print suggested queries
+    void print_tree(Node *root, std::string data);                  // overloaded print_tree method for recursion
+    void remove_prefix(std::string data);                           // remove prefix, if space in string
+    std::string getNo_prefix() { return noPrefix; }                 // return noPrefi
+    std::string break_string(std::string) {}                        // returns prefix
 };
+
+#endif
