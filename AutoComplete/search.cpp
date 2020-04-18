b2941acb50f69a918672bb5412281a0a6342aae6
Arya Nguyen <quynhnhu3132000@gmail.com>
2020-04-17 21:56:02 -0400

    Create search.cpp

---
+++ AutoComplete/search.cpp
@@ -0,0 +1,14 @@
+/****************************************************
+ * trie.cpp
+ * 
+ * Description: Trie Data Structure
+ * Author: Arya Nguyen
+ * Project: EyeTracking_SpeechGenerating
+ * 
+ ****************************************************/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <node.h>
+#include <trie.h>
