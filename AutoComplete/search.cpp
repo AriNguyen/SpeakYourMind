d2da951df98cab8810a16164cbd2f5932e06fc98
Arya Nguyen <49668914+AryaNguyen@users.noreply.github.com>
2020-04-18 00:02:48 -0400

    Update search.cpp

--- AutoComplete/search.cpp
+++ AutoComplete/search.cpp
@@ -1,7 +1,7 @@
 /****************************************************
- * trie.cpp
+ * search.cpp
  * 
- * Description: Trie Data Structure
+ * Description: Implement Trie Data Structure
  * Author: Arya Nguyen
  * Project: EyeTracking_SpeechGenerating
  * 
@@ -10,5 +10,45 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <node.h>
-#include <trie.h>
+#include <filesystem>
+
+#include "node.h"
+#include "trie.h"
+
+int main()
+{
+    std::string data;
+    std::fstream dictionary;
+    Trie complete_query;
+    std::string dict_path = "../data/words_dicitonary.json";
+
+    dictionary.open(dict_path, std::fstream::in); // read dictionary to trie data structure
+    if (dictionary.is_open())
+    {
+        while (dictionary)
+        {
+            dictionary >> data;
+            complete_query.insert(data);
+        }
+    }
+    else
+    {
+        std::cout << "erro: file not open." << std::endl;
+        return -1;
+    }
+    dictionary.close();
+    data.clear();
+
+    std::cout << "Enter search query: " << std::endl;
+    std::getline(std::cin, data); // use getline to catch spaces
+    complete_query.isSpace(data);
+    std::cout << "Your search options are: " << std::endl;
+    if (complete_query.getSpace() == true)
+    {
+        complete_query.remove_prefix(data);
+        complete_query.search(complete_query.break_string(data));
+    }
+    else
+        complete_query.search(data);
+    return 0;
+}
